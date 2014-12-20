d8f7d12d635c07910522d7f8423ec845f3257656
Prateek Saxena <prtksxna@gmail.com>
2014-12-20 18:11:05 +0530

    Initial commit

---
+++ fabric.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Fabric
+ *
+ * In the rhythm of life
+ *
+ * @link              https://github.com/prtksxna/fabric/
+ * @since             1.0.0
+ * @package           Fabric
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Fabric
+ * Plugin URI:        https://github.com/prtksxna/fabric/
+ * Description:       In the rhythm of life
+ * Version:           1.0.0
+ * Author:            Prateek Saxena & Siddharth Ashok
+ * Author URI:        https://github.com/prtksxna/fabric/graphs/contributors
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+?>
